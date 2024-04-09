68c32d5b7985771b5e28606a9385dc3d24068fc8
Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>
2024-04-09 16:55:08 +0200

    feat(infrastructure/remote_state): add output for client/tenant/sub_id
    Signed-off-by: Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>

---
+++ infrastructure/remote_state/data.tf
@@ -0,0 +1,3 @@
+# azurerm client config data source
+
+data "azurerm_client_config" "current" {}
--- infrastructure/remote_state/output.tf
+++ infrastructure/remote_state/output.tf
@@ -8,3 +8,18 @@ output "sa_name" {
 output "rg_name" {
   value = azurerm_resource_group.rg.name
 }
+
+# user assigned identity client id output
+output "ua_identity_client_id" {
+  value = azurerm_user_assigned_identity.workflowmsi.client_id
+}
+
+# user assigned identity tenant id output
+output "ua_identity_tenant_id" {
+  value = azurerm_user_assigned_identity.workflowmsi.tenant_id
+}
+
+# subscription id output
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
