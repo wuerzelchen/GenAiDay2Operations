c0a228a995e9ea0361142b6f84051315446e29b1
Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>
2024-04-09 17:13:10 +0200

    docs(infrastructure/remote_state): explained the config for fic
    Signed-off-by: Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>

--- infrastructure/remote_state/workflow-msi.tf
+++ infrastructure/remote_state/workflow-msi.tf
@@ -1,6 +1,7 @@
 locals {
   workflow_msi_name = "workflow-msi-${random_id.random.hex}"
-  fic_subject       = "repo:${var.fic_subject_repo}:ref:refs/heads/main"
+  # this is the subject of the federated identity credential, it is the repo and the branch that the token is scoped to
+  fic_subject = "repo:${var.fic_subject_repo}:ref:refs/heads/main"
 }
 
 resource "azurerm_user_assigned_identity" "workflowmsi" {
@@ -9,6 +10,7 @@ resource "azurerm_user_assigned_identity" "workflowmsi" {
   name                = local.workflow_msi_name
 }
 
+# This defines the federated identity credential that will be used to authenticate to the Azure AD Token Exchange endpoint, in our case relevant for github actions to authenticate via oauth2
 resource "azurerm_federated_identity_credential" "fic" {
   name                = "cicd-cred-${random_id.random.hex}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -19,8 +21,9 @@ resource "azurerm_federated_identity_credential" "fic" {
 }
 
 resource "azurerm_role_assignment" "example" {
-  # scope as "/subscriptions/data.azurerm_client_config.current.subscription_id"
+  # this sets the scope to the subscription level
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "Contributor"
-  principal_id         = azurerm_user_assigned_identity.workflowmsi.principal_id
+  # this sets the principal to the user assigned identity which gets the contributor role to the subscription(scope)
+  principal_id = azurerm_user_assigned_identity.workflowmsi.principal_id
 }
